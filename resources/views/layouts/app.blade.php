29dcfa07fb4ac1f51f6dd8ae2c6e087ada05a35b
Mohsen Mohammadi <Mohsen.mhm23@gmail.com>
2023-05-05 20:54:09 +0430

    Minor bugs fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,28 +33,35 @@
                          aria-expanded="false">
                 @else
                     <img src="/storage/avatars/{{auth()->user()->avatar ? : 'default-avatar.png'}}" alt="آواتار"
-                         style="width: 70px; border-radius: 100%" type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown"
-                         aria-expanded="false">
+                         style="width: 70px; border-radius: 100%" type="button" class="btn dropdown-toggle"
+                         data-bs-toggle="dropdown" aria-expanded="false">
                 @endguest
                 <ul class="dropdown-menu me-3" style="background: #1a202c">
                     @guest
                         @if (Route::has('login'))
-                            <li><a class="dropdown-item text-light text-center" href="{{ route('login') }}">ورود</a>
+                            <li>
+                                <a class="dropdown-item text-light text-center" href="{{ route('login') }}">ورود</a>
                             </li>
                         @endif
                         @if (Route::has('register'))
-                            <li><a class="dropdown-item text-light text-center" href="{{ route('register') }}">عضویت</a>
+                            <li>
+                                <a class="dropdown-item text-light text-center" href="{{ route('register') }}">عضویت</a>
                             </li>
                         @endif
                     @else
-                        <li><a class="dropdown-item text-light text-center"
-                               href="{{ route('home') }}">صفحه اصلی</a></li>
                         <li>
-                        <li><a class="dropdown-item text-light text-center"
-                               href="{{ route('profile.index') }}">پروفایل</a></li>
+                            <a class="dropdown-item text-light text-center" href="{{ route('home') }}">صفحه اصلی</a>
+                        </li>
+
+                        <li>
+                            <a class="dropdown-item text-light text-center"
+                               href="{{ route('profile.index') }}">پروفایل</a>
+                        </li>
+
                         <li>
                             <hr class="dropdown-divider">
                         </li>
+
                         <li><a class="dropdown-item text-light text-center" href="#" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">خروج</a></li>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
