2e338930301b960fdd1e96618efc8373bc1da770
Mohsen Mohammadi <Mohsen.mhm23@gmail.com>
2023-05-17 12:32:07 +0430

    Working on home page

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Song;
 use Illuminate\Http\Request;
 
 class SongController extends Controller
 {
     public function index($slug)
     {
-        dd($slug);
+        $song = Song::whereSlug($slug)->first();
+        return view('song.index', compact('song'));
     }
 }
