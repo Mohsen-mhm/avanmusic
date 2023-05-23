6f6cd9c57ca2c21d8b24b0c6392ddd68ac1c1d5f
Mohsen Mohammadi <Mohsen.mhm23@gmail.com>
2023-05-24 00:04:49 +0430

    Fix carousel on home page

--- resources/views/layouts/carousel.blade.php
+++ resources/views/layouts/carousel.blade.php
@@ -1,29 +1,24 @@
 <section class="container pt-0">
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-{{--            @php--}}
-{{--                $likedSongs = \App\Models\Like::select('song_id', \Illuminate\Support\Facades\DB::raw('COUNT(*) as like_count'))--}}
-{{--        ->groupBy('song_id')--}}
-{{--        ->orderByDesc('like_count')--}}
-{{--        ->limit(5)--}}
-{{--        ->get();--}}
-{{--                dd($likedSongs);--}}
-{{--            @endphp--}}
-            <div class="carousel-item active">
-                <a href="./pages/music.html">
-                    <img src="images/zedbazi.jpg" class="col  d-block img-fluid ax w-100 opacity-100" alt="...">
-                </a>
-            </div>
-            <div class="carousel-item">
-                <a href="./pages/music.html">
-                    <img src="images/maxresdefault.jpg" class="col d-block img-fluid ax w-100" alt="...">
-                </a>
-            </div>
-            <div class="carousel-item">
-                <a href="./pages/music.html">
-                    <img src="images/maxresdefault%20(1).jpg" class="col d-block img-fluid ax w-100" alt="...">
-                </a>
-            </div>
+            @php
+                $likedSongs = \App\Models\Like::select('song_id', \Illuminate\Support\Facades\DB::raw('COUNT(*) as like_count'))
+                    ->groupBy('song_id')
+                    ->orderByDesc('like_count')
+                    ->limit(5)
+                    ->get();
+            @endphp
+            @foreach($likedSongs as $likedSong)
+                @php
+                    $song = \App\Models\Song::find($likedSong->song_id);
+                @endphp
+                <div class="carousel-item @if($loop->first) active @endif">
+                    <a href="{{ route('song', $song) }}">
+                        <img src="/storage/song-covers/{{ $song->cover }}"
+                             class="col d-block img-fluid ax w-100 opacity-100" alt="...">
+                    </a>
+                </div>
+            @endforeach
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
                 data-bs-slide="prev">
