94c2ad583e5397592053dece0ce93ccd76c763f3
Mohsen Mohammadi <Mohsen.mhm23@gmail.com>
2023-06-01 03:05:53 +0430

    Minor bugs fixed

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -2,13 +2,14 @@
 
 @section('content')
     <h2 class="text-light text-center">اتصال به درگاه پرداخت</h2>
-    <div class="row" dir="rtl">
-        <div class="col-md-6 d-flex flex-column justify-content-center align-items-center text-light mt-5">
+    <div class="row d-flex justify-content-center align-items-center text-light" dir="rtl">
+        <div class="col-md-6 d-flex flex-column justify-content-center align-items-center mt-5">
             <p>قیمت نهایی: <b class="text-warning">{{ $order->price }}</b> تومان</p>
             <p>درگاه پرداخت:</p>
             <img src="/images/ssl-logo-1.png" alt="zarinpal logo" style="min-width: 200px; max-width: 300px">
         </div>
-        <div class="col-md-4 d-flex flex-column justify-content-center align-items-center text-light mt-5">
+
+        <div class="col-md-6 d-flex flex-column justify-content-center align-items-center mt-5">
             <p>عملیات:</p>
             <button class="btn btn-success mt-2 mb-2" onclick="document.querySelector('#payment-success').submit()">
                 پرداخت
