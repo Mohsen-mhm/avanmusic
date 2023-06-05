e8ce61356714f9eb62469a2d833b99ca609d480f
Mohsen Mohammadi <Mohsen.mhm23@gmail.com>
2023-06-05 12:03:49 +0430

    Minor bugs fixed

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,25 +5,25 @@
         <div class="row text-light">
             <div class="col-md-6 p-4 p-sm-5 order-2 order-sm-1">
                 <small class="text-uppercase text-warning"></small>
-                <h1 class="h2 mb-4" style="font-weight: 600;">Our <span class="text-warning">mission</span></h1>
+                <h1 class="h2 mb-4" style="font-weight: 600;"><span class="text-warning">هدف</span> ما</h1>
                 <p class="text-light" style="line-height: 2;">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
                 </p>
 
             </div>
             <div class="col-md-6 p-0 text-center order-1 order-sm-2">
                 <img
-                    src="https://images.pexels.com/photos/2467506/pexels-photo-2467506.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                    class="w-100" alt="">
+                    src="/images/team1.png"
+                    class="w-75" alt="">
             </div>
 
             <div class="col-md-6 p-0 text-center">
                 <img
-                    src="https://images.pexels.com/photos/2467506/pexels-photo-2467506.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                    class="w-100" alt="">
+                    src="/images/team2.png"
+                    class="w-75" alt="">
             </div>
             <div class="col-md-6 p-4 p-sm-5">
                 <small class="text-uppercase text-warning"></small>
-                <h1 class="h2 mb-4" style="font-weight: 600;">About<span class="text-warning"> us</span></h1>
+                <h1 class="h2 mb-4" style="font-weight: 600;"><span class="text-warning"> درباره</span> ما</h1>
                 <p class="text-light" style="line-height: 2;">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
 
             </div>
