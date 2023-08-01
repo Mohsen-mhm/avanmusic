9e3ef8e1b24997a89c039c348b80f43a1c49646a
Mohsen Mohammadi <Mohsen.mhm23@gmail.com>
2023-08-01 11:50:51 +0430

    Minor bugs fixed

--- resources/views/layouts/second.blade.php
+++ resources/views/layouts/second.blade.php
@@ -4,7 +4,12 @@
         <div class="row text-center text-light d-flex justify-content-center align-items-center mb-4">
             <div class="col-md-10 ml-auto mr-auto">
                 <h2 class="title mb-3 text-warning mt-2">بهترین تجربه</h2>
-                <h5 class="description" style="font-size: 16px; line-height: 2rem">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.</h5>
+                <h5 class="description" style="font-size: 16px; line-height: 2rem">
+                    با آوان موزیک، جهانی از صداها و احساسات را کشف کنید. آهنگ‌های شگفت‌انگیز، هنرمندان برجسته، و
+                    آلبوم‌های متنوع در انتظار شما هستند. همین حالا به جذابیت و هیجان موسیقی بپیوندید و بهترین تجربه
+                    موزیک را تجربه کنید. آوان موزیک، جایی که هنر موسیقی با شما دوست شده و هر نتی احساسی نو رقصان می‌شود.
+                    به جهانی از صداها و همدلی موسیقی پیوسته و امروز همراه آوان موزیک باشید.
+                </h5>
             </div>
         </div>
         <div class="section d-flex justify-content-center align-items-center text-center text-light">
@@ -18,7 +23,9 @@
                                 <img src="/images/microphone.png" alt="artist" class="w-50 my-3">
                             </div>
                             <h4 class="info-title">خواننده</h4>
-                            <p style="font-size: 14px; line-height: 2rem">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                            <p style="font-size: 14px; line-height: 2rem">
+                                برترین خواننده ها را دنبال کنید
+                            </p>
                         </div>
                     </div>
 
@@ -28,7 +35,9 @@
                                 <img src="/images/radio.png" alt="artist" class="w-50 my-3">
                             </div>
                             <h4 class="info-title">همراه</h4>
-                            <p style="font-size: 14px; line-height: 2rem">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                            <p style="font-size: 14px; line-height: 2rem">
+                                همیشه و همه جا به صورت آنلاین گوش دهید
+                            </p>
                         </div>
                     </div>
 
@@ -38,7 +47,9 @@
                                 <img src="/images/handphones.png" alt="artist" class="w-50 my-3">
                             </div>
                             <h4 class="info-title">موزیک</h4>
-                            <p style="font-size: 14px; line-height: 2rem">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                            <p style="font-size: 14px; line-height: 2rem">
+                                به روز ترین موزیک ها را برای شما داریم
+                            </p>
                         </div>
                     </div>
                 </div>
