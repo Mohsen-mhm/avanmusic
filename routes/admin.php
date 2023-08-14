eccfa01bfdd830ca12b6791c739d3233fa5f98f3
Mohsen Mohammadi <67678628+Mohsen-mhm@users.noreply.github.com>
2023-08-14 11:48:41 +0330

    Minor bugs fixed

--- routes/admin.php
+++ routes/admin.php
@@ -22,7 +22,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', [AdminController::class, 'home'])->name('home');
 
 Route::resource('users', UserController::class)->except(['show', 'destroy']);
